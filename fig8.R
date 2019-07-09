c486f9af94fa81a05039337fd6fd9c7028b63eb1
Kaycee Coleman <kaycee.coleman@gmail.com>
2019-07-09 10:51:10 -0400

    Create fig8.R

---
+++ fig8.R
@@ -0,0 +1,28 @@
+# This is the combined plots for 
+# Marine mammal, bird, and fish diet literature
+
+#--------------#
+# load packages
+#--------------#
+require(dplyr)
+require(ggplot2)
+require(xlsx)
+#--------------#
+
+#--------------#
+# load data
+#--------------#
+# data is downloaded from google drive full_manuscript/all_tables
+#--------------#
+
+#--------------#
+# format data
+#--------------#
+#--------------#
+
+#--------------#
+# plot
+#--------------#
+ggplot()+geom_point()
+#--------------#
+
